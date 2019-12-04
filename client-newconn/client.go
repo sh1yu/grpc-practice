3df9ec914359bfce884fd9fe3f53a51859fa9cc6
sypeng <sypeng@iflytek.com>
2019-12-04 12:25:12 +0800

    add client with new conn for each request

---
+++ client-newconn/client.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+    "context"
+    "flag"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/credentials"
+    "google.golang.org/grpc/metadata"
+    "grpc-practice/hello"
+    "io"
+    "log"
+    "os"
+    "strconv"
+    "strings"
+    "sync"
+    "time"
+)
+
+var (
+    tls           = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+    caFile        = flag.String("crt", "", "The file containing the CA root cert file")
+    serverAddr    = flag.String("addr", "psy.cn:443", "The server address in the format of host:port")
+    crtServerName = flag.String("host", "psy.cn", "The server name use to verify the hostname returned by TLS handshake")
+    c             = flag.Int("c", 5, "concurrent")
+    n             = flag.Int("n", 10000, "request for each concurrent")
+    s             = flag.Int("s", 500, "sleep milliseconds between each request")
+)
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+    return "canary: always"
+}
+
+func (i *arrayFlags) Set(value string) error {
+    *i = append(*i, value)
+    return nil
+}
+
+var headerFlags arrayFlags
+
+func main() {
+    flag.Var(&headerFlags, "H", "grpc request header")
+    flag.Parse()
+    log.SetOutput(os.Stdout)
+
+    headerKvs := make([]string, 0, 2*len(headerFlags))
+    for _, v := range headerFlags {
+        token := strings.Split(v, ":")
+        if len(token) != 2 {
+            continue
+        }
+        headerKvs = append(headerKvs, strings.TrimSpace(token[0]), strings.TrimSpace(token[1]))
+    }
+
+    wg := sync.WaitGroup{}
+    for i := 0; i < *c; i++ {
+        wg.Add(1)
+        go func(index int) {
+            for i := 0; i < *n; i++ {
+                doRequest(headerKvs, index)
+                <-time.After(time.Duration(*s) * time.Millisecond)
+            }
+            wg.Done()
+        }(i)
+    }
+    wg.Wait()
+}
+
+func doRequest(headerKvs []string, index int) {
+    var opts []grpc.DialOption
+    if *tls {
+        //with tls
+        if *caFile == "" {
+            *caFile = "crt/cert.pem"
+        }
+        creds, err := credentials.NewClientTLSFromFile(*caFile, *crtServerName)
+        if err != nil {
+            log.Fatalf("Failed to create TLS credentials %v", err)
+        }
+        opts = append(opts, grpc.WithTransportCredentials(creds))
+    } else {
+        //without tls
+        opts = append(opts, grpc.WithInsecure())
+    }
+
+    opts = append(opts, grpc.WithBlock())
+    ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+    conn, err := grpc.DialContext(ctx, "passthrough:///"+*serverAddr, opts...)
+    if err != nil {
+        log.Fatalf("fail to dial: %v", err)
+    }
+
+    defer func() {
+        _ = conn.Close()
+    }()
+    client := hello.NewHelloServiceClient(conn)
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+    metaCtx := metadata.AppendToOutgoingContext(ctx, headerKvs...)
+    stream, err := client.SayHello(metaCtx)
+    if err != nil {
+        log.Fatalf("[%v]  %v.SayHello, %v", index, client, err)
+    }
+    waitc := make(chan struct{})
+    go func() {
+        for {
+            in, err := stream.Recv()
+            if err == io.EOF {
+                close(waitc)
+                return
+            }
+            if err != nil {
+                log.Printf("[%v]  Failed to receive a reply : %v", index, err)
+                close(waitc)
+                return
+            }
+            log.Printf("[%v] Got message %s ", index, in.Reply)
+        }
+    }()
+    for i := 0; i < 1; i++ {
+        if err := stream.Send(&hello.HelloRequest{Greeting: "good morning! " + strconv.Itoa(i)}); err != nil {
+            log.Fatalf("Failed to send a request: %v", err)
+        }
+        //<-time.After(1 * time.Millisecond)
+    }
+    _ = stream.CloseSend()
+    <-waitc
+}
